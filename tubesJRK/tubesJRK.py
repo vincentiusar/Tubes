7952e31a22ffaf860bcd16b2a7b29d6fbc297e30
Vincentius Ar <58662886+vincentiusar@users.noreply.github.com>
2021-06-04 19:06:06 +0700

    Add files via upload

--- tubesJRK/tubesJRK.py
+++ tubesJRK/tubesJRK.py
@@ -1,63 +1,63 @@
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import Node
 from mininet.log import setLogLevel, info
 from mininet.cli import CLI
-from mininet.link import TCLink
-from mininet.node import CPULimitedHost
+from mininet.link import TCLink, Intf
+from mininet.node import CPULimitedHost, Node, Controller
 import time
 import os
 
-class MyTopo ( Topo ):
-    def __init__( self, **opts ):
-    Topo.__init__( self, **opts )
-    # Membuat objek host
-    H1 = self.addHost( 'H1', ip='10.0.0.1/24' )
-    H2 = self.addHost( 'H2', ip='10.0.0.2/24' )
-    H3 = self.addHost( 'H3', ip='10.0.0.3/24' )
-    H4 = self.addHost( 'H4', ip='10.0.0.4/24' )
+class LinuxRouter(Node):
+    def config(self, **params):
+        super(LinuxRouter, self).config(**params)
+        self.cmd('sysctl net.ipv4.ip_forward=1')
 
-    # Membuat objek switch
-    S1 = self.addSwitch( 'S1' )
-    S2 = self.addSwitch( 'S2' )
-    S3 = self.addSwitch( 'S3' )
-    S4 = self.addSwitch( 'S4' )
-    S5 = self.addSwitch( 'S5' )
-    # Membuat link antar objek
-    self.addLink( H1, S2 )
-    self.addLink( H2, S3 )
-    self.addLink( H3, S4 )
-    self.addLink( H4, S5 )
-    self.addLink( S1, S2, bw=50 )
-    self.addLink( S1, S3, bw=50 )
-    self.addLink( S1, S4, bw=50 )
-    self.addLink( S1, S5, bw=10 )
+    def terminate(self):
+        self.cmd('sysctl net.ipv4.ip_forward=0')
+        super(LinuxRouter, self).terminate()
 
-def runTopo():
-    # Memastikan mininet bersih dari cache sebelumnya
-    os.system('mn –c')
-    # Membangun Topologi
-    topo = MyTopo()
-    net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
-    net.start()
-    # Konfigurasi congestion control
-    print(net['H1'].cmd('sysctl –w net.ipv4.tcp_congestion_control=[reno/vegas]'))
-    print(net['H2'].cmd('sysctl –w net.ipv4.tcp_congestion_control=[reno/vegas]'))
-    print(net['H3'].cmd('sysctl –w net.ipv4.tcp_congestion_control=[reno/vegas]'))
-    print(net['H4'].cmd('sysctl –w net.ipv4.tcp_congestion_control=[reno/vegas]'))
-    # Memasukan objek host pada variabel
-    h1, h2, h3, h4 = net.get ('H1', 'H2', 'H3', 'H4')
-    # Menjalankan iPerf dibackground process
-    h4.cmd('iperf –s&')
-    h2.cmd('iperf –c 10.0.0.4&')
-    h3.cmd('iperf –c 10.0.0.4&')
-    h1.cmd('iperf –c 10.0.0.4 –i 1&')
-    time.sleep(15)
-    # Menampilkan hasil iPerf dari H1 ke H4
-    h1.cmdPrint('fg')
-    CLI(net)
-    net.stop()
+class MyTopo(Topo) :
+	def __init__(self, **opts) :
+		Topo.__init__(self, **opts)
+		
+		HA = self.addHost('HA', ip='192.168.1.1/24', mac='13:01:19:02:21:00:00:01')
+		HB = self.addHost('HB', ip='192.168.1.2/24', mac='13:01:19:02:21:00:00:02')
+		
+		R1 = self.addHost('R1', cls=LinuxRouter, ip='192.168.0.1/29')
+		R2 = self.addHost('R2', cls=LinuxRouter, ip='192.168.0.2/29')
+		R3 = self.addHost('R3', cls=LinuxRouter, ip='192.168.0.3/29')
+		R4 = self.addHost('R4', cls=LinuxRouter, ip='192.168.0.4/29')
 
-if __name__== '__main__':
-    setLogLevel('info')
-    runTopo()
+		self.addLink( HA, R1, bw=1, intfName2='R1-eth1' )
+		self.addLink( HA, R2, bw=1, intfName2='R2-eth1' )
+		self.addLink( HB, R3, bw=1, intfName2='R3-eth1' )
+		self.addLink( HB, R4, bw=1, intfName2='R4-eth1' )
+		self.addLink( R1, R3, bw=0.5, intfName1='R1-eth2', intfName2='R3-eth2' )
+		self.addLink( R1, R4, bw=1, intfName1='R1-eth3', intfName2='R4-eth2' )
+		self.addLink( R2, R3, bw=1, intfName1='R2-eth2', intfName2='R3-eth3' )
+		self.addLink( R2, R4, bw=0.5, intfName1='R2-eth3', intfName2='R4-eth3' )
+	
+def runTopo() :
+	os.system('mn -c')
+	
+	topo = MyTopo()
+	net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
+	net.start()
+	
+	HA, HB = net.get('HA', 'HB')
+	
+	print(net['HA'].cmd('sysctl -w net.ipv4.tcp_congestion_control=cubic'))
+	print(net['HB'].cmd('sysctl -w net.ipv4.tcp_congestion_control=cubic'))
+	
+	print(HB.cmd('iperf -s&'))
+	print(HA.cmd('iperf -c 192.168.1.2 -i 1&'))
+	
+	time.sleep(2)
+	
+	HA.cmd('fg')
+	CLI(net)
+	net.stop()
+	
+if __name__=='__main__' :
+	setLogLevel('info')
+	runTopo()
